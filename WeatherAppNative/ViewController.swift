f1f30e162464041b29458d61dc6719337a22d3ee
alekseeva8 <alekseeva.ev8@gmail.com>
2020-09-28 10:43:00 +0300

    Initial Commit

---
+++ WeatherAppNative/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppNative
+//
+//  Created by Elena Alekseeva on 9/28/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
